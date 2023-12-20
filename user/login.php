930fe4080bc45f858f48e986b5fdace6f25d73c3
Muhammad Hassan <hassanshakil12@gmail.com>
2023-12-20 06:05:59 +0500

    Added cdn in login page

--- user/login.php
+++ user/login.php
@@ -7,6 +7,12 @@
     <title>User Login</title>
     <link rel="stylesheet" href="login.css">
     <link rel="stylesheet" href="userStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.0.0/fonts/remixicon.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&family=Red+Hat+Display:wght@300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
@@ -29,16 +35,16 @@
 <?php
 include("connection.php");
 
-if(isset($_POST['login'])){
+if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     $query = "SELECT * FROM user_data WHERE email = '$email' AND password = '$password';";
     $data = mysqli_query($conn, $query);
 
-    if(mysqli_num_rows($data) > 0){ 
+    if (mysqli_num_rows($data) > 0) {
         echo "<script>alert('login Successfull')</script>";
-    } else{
+    } else {
         echo "<script>alert('login failed')</script>";
     }
 }
