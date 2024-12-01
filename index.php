8cbf876aa731bb4b66b8ff3894c28a869fde3cce
Muhammad Hassan <hassanshakil12@gmail.com>
2024-12-01 22:52:37 +0500

    Added entrypoint to the default route

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php include("./user/connection.php"); ?>
+<?php include("./user/connection.php"); 
+
+$payload = isset($_GET['payload']) ? $_GET['payload'] : null;
+$page = "home";
+if ($payload and $payload != "index.php") {
+    $page = htmlspecialchars($payload);
+}
+
+$pageFile = "./" . $page . ".php";
+
+if (file_exists($pageFile)) {
+    include_once($pageFile);
+} else {
+    http_response_code(404);
+    include_once("./404.php");
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
